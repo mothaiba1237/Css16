18a48402bc768d3873db3b6cfb69f00108ab93fd
Võ Thanh Nguyên <nguyenvo9955@gmail.com>
2025-01-08 17:06:34 +0700

    Add files via upload

--- ss16bt2.cpp
+++ ss16bt2.cpp
@@ -1,14 +1,19 @@
 #include <stdio.h>
 
+int swap(int *ptr1, int *ptr2) {
+    int temp = *ptr1;
+    *ptr1 = *ptr2;
+    *ptr2 = temp;
+    return 0;
+}
+
 int main() {
     int x = 1;
     int y = 2;
     int *ptr1 = &x;
     int *ptr2 = &y;
     printf("x = %d, y = %d\n", x, y);
-    int temp = *ptr1;
-    *ptr1 = *ptr2;
-    *ptr2 = temp;
+    swap(ptr1, ptr2);
     printf("x = %d, y = %d\n", x, y);
     return 0;
 }
