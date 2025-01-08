c26ed2eac67f8f53d251437028a8649e81f4ae8a
Võ Thanh Nguyên <nguyenvo9955@gmail.com>
2025-01-08 17:09:09 +0700

    Add files via upload

--- ss16bt5.cpp
+++ ss16bt5.cpp
@@ -1,18 +1,19 @@
 #include <stdio.h>
 
-int* upd(int a[], int n, int v, int p) {
-    if (p >= 0 && p < n) a[p] = v;
-    return a;
+int* updateArray(int arr[], int size, int value, int position) {
+    if (position >= 0 && position < size) arr[position] = value;
+    return arr;
 }
 
 int main() {
-    int a[] = {1, 3, 5, 7, 9}, n = sizeof(a) / sizeof(a[0]);
-    printf("Truoc: ");
-    for (int i = 0; i < n; i++) printf("%d ", a[i]);
+    int numbers[] = {1, 3, 5, 7, 9};
+    int size = sizeof(numbers) / sizeof(numbers[0]);
+    printf("truoc: ");
+    for (int i = 0; i < size; i++) printf("%d ", numbers[i]);
     printf("\n");
-    int* b = upd(a, n, 33, 2);
-    printf("Sau: ");
-    for (int i = 0; i < n; i++) printf("%d ", b[i]);
+    int* updatedArray = updateArray(numbers, size, 33, 2);
+    printf("sau: ");
+    for (int i = 0; i < size; i++) printf("%d ", updatedArray[i]);
     printf("\n");
     return 0;
 }
